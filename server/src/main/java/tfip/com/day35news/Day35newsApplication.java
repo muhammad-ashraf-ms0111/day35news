075e83891cee71bce75ba5b7d668d1e2830c3e6d
Muhammad-Ashraf-MS0111 <muhammad.ashraf.ms0111@gmail.com>
2023-03-28 15:37:44 +0800

    first commit

---
+++ server/src/main/java/tfip/com/day35news/Day35newsApplication.java
@@ -0,0 +1,13 @@
+package tfip.com.day35news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day35newsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day35newsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/tfip/com/day35news/Day35newsApplicationTests.java
@@ -0,0 +1,13 @@
+package tfip.com.day35news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day35newsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
